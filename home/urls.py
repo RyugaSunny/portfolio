fee6af2e2489ced8e17317e49971daf9c035111c
Deepanshu <91669316+RyugaSunny@users.noreply.github.com>
2024-04-01 20:51:23 +0530

    revisiting

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index),
-    path('portfolio-details/', views.portfolio_details, name='portfolio-details'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(r):
     return render(r,"index.html")
-
-def portfolio_details(r):
-    return render(r,"portfolio-details.html")
