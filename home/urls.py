f303558c622ba567eece978bc909e4dfabed3829
Deepanshu <91669316+RyugaSunny@users.noreply.github.com>
2024-04-03 23:47:00 +0530

    fixed in app linking

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from home import views
 
 urlpatterns = [
     path('', views.index),
+    path('oldindex/', views.oldindex, name='oldindex'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(r):
     return render(r,r"home/index.html")
+
+def oldindex(r):
+    return render(r,r"oldwebsite/index.html")
